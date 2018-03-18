387aaffc5927ed9795fab1543bcbc1a3be60545a
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2018-03-18 10:52:33 -0600

    add adding heart functionality

--- src/components/layout/Card.js
+++ src/components/layout/Card.js
@@ -4,15 +4,28 @@ import heart from '../../assets/hearts.png'
 
 class Card extends Component {
 
+    state = {
+        heartCount: 0,
+    };
+
+    addToHeartCount = () => {
+        this.setState((prevState) => ({heartCount: prevState.heartCount + 1}))
+    };
+
     render() {
         return (
             <div className="card">
-                {/*<div>*/}
-                    {/*<img src={heart} alt=""/>*/}
-                {/*</div>*/}
+                <div className="heart-container">
+                    <img onClick={this.addToHeartCount} src={heart} alt=""/>
+                    <div className="heart-count">
+                        {this.state.heartCount}
+                    </div>
+                </div>
 
-                <div>
-                    {this.props.value}
+                <div className="card-text">
+                    <div>
+                        {this.props.value}
+                    </div>
                 </div>
             </div>
         )
