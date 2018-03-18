7f473dc57af63c7fba07f8e86962ea4752649e4e
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2018-03-18 10:08:34 -0600

    add input and card styling

--- src/components/layout/Card.js
+++ src/components/layout/Card.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './Card.css';
+import heart from '../../assets/hearts.png'
 
 class Card extends Component {
 
     render() {
         return (
             <div className="card">
-                {this.props.value}
+                {/*<div>*/}
+                    {/*<img src={heart} alt=""/>*/}
+                {/*</div>*/}
+
+                <div>
+                    {this.props.value}
+                </div>
             </div>
         )
     }
